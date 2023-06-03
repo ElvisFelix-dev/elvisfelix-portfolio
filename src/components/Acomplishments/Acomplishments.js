501a9dc946f35383c5a35460624bd389f7578975
Elvis Felix <elvisjaspion@gmail.com>
2023-06-03 01:41:51 -0300

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -5,14 +5,13 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
   { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 7, text: 'Github Followers', },
+  { number: 33, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Achievements</SectionTitle>
+    <SectionTitle>Arquivo Pessoal</SectionTitle>
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
