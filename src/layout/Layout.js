23bc34ffe4f5c315d53c973afdde4b36c4ff092c
Elvis Felix <elvisjaspion@gmail.com>
2023-06-03 01:38:33 -0300

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
+import Acomplishments from '../components/Acomplishments/Acomplishments'
 import { Container } from './LayoutStyles'
 
 export const Layout = ({children}) => {
   return (
     <Container>
      <Header/>
-     <main>{children}</main> 
+     <main>{children}</main>
+      <Acomplishments />
      <Footer/>
     </Container>
   )
